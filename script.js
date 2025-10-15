01ba9ebac387a6bcc0a236bdee01fe01f5f1de48
Charan Reddy <154213087+KUNAMMANISAICHARANREDDY@users.noreply.github.com>
2025-10-15 23:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let getTimer=document.getElementById("timer");
+let curDate=new Date();
+
+setInterval(
+	function () {
+		curDate=new Date();
+		getTimer.innerHTML=curDate.toLocaleString();
+	},1000
+)
